11c27f6b3adc5b1dd93f1d8f5b4a4cdb703c5241
Deepika67-nm <deepika.k2026@vitstudent.ac.in>
2026-08-30 14:48:30 +0530

    Update if_else.py

--- if_else.py
+++ if_else.py
@@ -1,5 +1,5 @@
-Problem:#https://www.hackerrank.com/challenges/py-if-else/problem?isFullScreen=true
-Solved: 30 Aug 2026
+#Problem:https://www.hackerrank.com/challenges/py-if-else/problem?isFullScreen=true
+#Solved: 30 Aug 2026
 n=int(input())
 if(n%2==1):
     print("Weird")
@@ -7,9 +7,8 @@ elif(n%2==0 and 2<=n<=5):
     print("Not Weird")
 elif(n%2==0 and 6<=n<=20):
     print("Weird")
-elif(n%2==0 and n>20):
-    print("Not Weird")
 else:
-    print()
+    print("Not Weird")
+    
     
 
